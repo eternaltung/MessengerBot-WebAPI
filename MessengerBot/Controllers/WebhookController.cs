7b5041f5926f1cd3799d13c4fb2c88bb7f4e96e0
eternaltung <eternaltung@gmail.com>
2016-05-08 23:13:29 +0800

    add webhook api

---
+++ MessengerBot/Controllers/WebhookController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using MessengerBot.Models;
+
+namespace MessengerBot.Controllers
+{
+    public class WebhookController : ApiController
+    {
+        string pageToken = "your fb page token";
+
+        public HttpResponseMessage Get()
+        {
+            var querystrings = Request.GetQueryNameValuePairs().ToDictionary(x => x.Key, x => x.Value);
+            if (querystrings["hub.verify_token"] == "hello")
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(querystrings["hub.challenge"], Encoding.UTF8, "text/plain")
+                };
+            }
+            return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody]WebhookModel value)
+        {
+            foreach (var item in value.entry[0].messaging)
+            {
+                if (item.message == null)
+                    break;
+                else
+                    await SendMessage(GetMessageTemplate(item.message.text, item.sender.id));
+            }
+        }
+
+        /// <summary>
+        /// get text message template
+        /// </summary>
+        /// <param name="text">text</param>
+        /// <param name="sender">sender id</param>
+        /// <returns>json</returns>
+        private string GetMessageTemplate(string text, string sender)
+        {
+            return $@"
+{{
+recipient: {{id: ""{sender}"" }},
+message: {{ text: ""{text}"" }}
+}}";
+        }
+
+        /// <summary>
+        /// send message
+        /// </summary>
+        /// <param name="json">json</param>
+        private async Task SendMessage(string json)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.PostAsync($"https://graph.facebook.com/v2.6/me/messages?access_token={pageToken}", new StringContent(json, Encoding.UTF8, "application/json"));
+            }
+        }
+    }
+}
+
---
+++ MessengerBot/Models/WebhookModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MessengerBot.Models
+{
+    public class WebhookModel
+    {
+        [JsonProperty("object")]
+        public string _object { get; set; }
+        public List<Entry> entry { get; set; }
+    }
+
+    public class Entry
+    {
+        public string id { get; set; }
+        public long time { get; set; }
+        public List<Messaging> messaging { get; set; }
+    }
+
+    public class Messaging
+    {
+        public Sender sender { get; set; }
+        public Recipient recipient { get; set; }
+        public long timestamp { get; set; }
+        public Message message { get; set; }
+        public Postback postback { get; set; }
+    }
+
+    public class Postback
+    {
+        public string payload { get; set; }
+    }
+
+    public class Sender
+    {
+        public string id { get; set; }
+    }
+
+    public class Recipient
+    {
+        public string id { get; set; }
+    }
+
+    public class Message
+    {
+        public string mid { get; set; }
+        public int seq { get; set; }
+        public string text { get; set; }
+    }
+}
